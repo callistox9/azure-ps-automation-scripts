e7e1880d6851242965c4d02fa2de5a1e7958ab5f
Soumya Sourav Guha <codecallisto@gamil.com>
2024-05-09 00:00:10 +0530

    vp_script

--- VPN_IaC_powershell/Vpn_powershell_script.ps1
+++ VPN_IaC_powershell/Vpn_powershell_script.ps1
@@ -1,12 +1,17 @@
-#powershell Script to deploy virtual network gateway in azure
+#Powershell Script to deploy virtual network gateway in azure
+#Connect Az Account
+Import-Module Az.Resources
+Connect-AzAccount -UseDeviceAuthentication -SubscriptionId "xxxx-xxxx-xxxx"
+
+#create resource group if does not exist
 New-AzResourceGroup -Name TestRG1 -Location EastUS
 
+#Create Virtual Network And Add Subnet
 $virtualNetwork = New-AzVirtualNetwork -ResourceGroupName TestRG1 -Location EastUS -Name VNet1 -AddressPrefix 10.1.0.0/16
-
-$subnetConfig = Add-AzVirtualNetworkSubnetConfig -Name Frontend -AddressPrefix 10.1.0.0/24 -VirtualNetwork $virtualNetwork
-
-$subnetConfig = Add-AzVirtualNetworkSubnetConfig -Name Frontend -AddressPrefix 10.1.0.0/24 -VirtualNetwork $virtualNetwork
-
+Add-AzVirtualNetworkSubnetConfig -Name Frontend -AddressPrefix 10.1.0.0/24 -VirtualNetwork $virtualNetwork
 $virtualNetwork | Set-AzVirtualNetwork
 
+
 $vnet = Get-AzVirtualNetwork -ResourceGroupName TestRG1 -Name VNet1
+Add-AzVirtualNetworkSubnetConfig -Name 'GatewaySubnet' -AddressPrefix 10.1.255.0/27 -VirtualNetwork $vnet
+$vnet | Set-AzVirtualNetwork
