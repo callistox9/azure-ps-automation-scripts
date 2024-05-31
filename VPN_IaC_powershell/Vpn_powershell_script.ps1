e9653b581832c6478b8f47ef8408d874fd5d5399
Soumya Sourav Guha <codecallisto@gamil.com>
2024-05-31 18:51:23 +0530

    organised the script using all variable

--- VPN_IaC_powershell/Vpn_powershell_script.ps1
+++ VPN_IaC_powershell/Vpn_powershell_script.ps1
@@ -1,32 +1,46 @@
-#Powershell Script to deploy virtual network gateway in azure
-#Connect Az Account
+# Organized script to deploy virtual network gateway in azure
+
+# Import required modules
 Import-Module Az.Resources
 Import-Module Az.Network
-Connect-AzAccount -UseDeviceAuthentication -SubscriptionId "xxxx-xxxx-xxxx"
-#Double check subscription, just to be sure, cause I have messed up multiple times
-set-AzContext -Subscription "xxxx-xxxx-xxxx"
-#create resource group if does not exist
-New-AzResourceGroup -Name TestRG1 -Location EastUS
+$SubscriptionId="xxxx-xxxx-xxxx"
+# Connect to azure account
+Connect-AzAccount -UseDeviceAuthentication -SubscriptionId $SubscriptionId
+# Verify the subscription
+Set-AzContext -Subscription $SubscriptionId
 
-#Create Virtual Network And Add Subnet, if does not exist
-$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName TestRG1 -Location EastUS -Name VNet1 -AddressPrefix 10.1.0.0/16 
-Add-AzVirtualNetworkSubnetConfig -Name 'GatewaySubnet' -AddressPrefix 10.1.255.0/27 -VirtualNetwork $virtualNetwork | Set-AzVirtualNetwork
+# Create resource group if does not exist
+$ResourceGroupName = "TestRG1"
+$Location = "EastUS"
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
 
-#OR get the Virtual network of your requirement and add gatewaySubnet, which is required for Virtual Network Gateway Services.
+# Create Virtual Network And Add Subnet, if does not exist
+$VirtualNetworkName = "VNet1"
+$AddressPrefix = "10.1.0.0/16"
+$SubnetName = "GatewaySubnet"
+$SubnetAddressPrefix = "10.1.255.0/27"
+$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Location $Location -Name $VirtualNetworkName -AddressPrefix $AddressPrefix
+Add-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix -VirtualNetwork $virtualNetwork | Set-AzVirtualNetwork
 
-$vnet = Get-AzVirtualNetwork -ResourceGroupName TestRG1 -Name VNet1
-Add-AzVirtualNetworkSubnetConfig -Name 'GatewaySubnet' -AddressPrefix 10.1.255.0/27 -VirtualNetwork $vnet | Set-AzVirtualNetwork
-#NOTE - The name of subnet should be exactly the same as it is necessary for VPN to work
+# NOTE - The name of subnet should be exactly the same as it is necessary for VPN to work
 
-# PublicIPAdress
-$gwpip = New-AzPublicIpAddress -Name "VNet1GWIP" -ResourceGroupName "TestRG1" -Location "EastUS" -AllocationMethod Static
+# PublicIPAddress
+$PublicIpAddressName = "VNet1GWIP"
+$gwpip = New-AzPublicIpAddress -Name $PublicIpAddressName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Static
 
 # Creating GatewayIp address configuration
-$vnet = Get-AzVirtualNetwork -Name VNet1 -ResourceGroupName TestRG1
-$subnet = Get-AzVirtualNetworkSubnetConfig -Name 'GatewaySubnet' -VirtualNetwork $vnet
-$gwipconfig = New-AzVirtualNetworkGatewayIpConfig -Name gwipconfig1 -SubnetId $subnet.Id -PublicIpAddressId $gwpip.Id
+$vnet = Get-AzVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName
+$subnet = Get-AzVirtualNetworkSubnetConfig -Name $SubnetName -VirtualNetwork $vnet
+$GatewayIpConfigName = "gwipconfig1"
+$gwipconfig = New-AzVirtualNetworkGatewayIpConfig -Name $GatewayIpConfigName -SubnetId $subnet.Id -PublicIpAddressId $gwpip.Id
 
 # Creating VPN gateway
-New-AzVirtualNetworkGateway -Name VNet1GW -ResourceGroupName TestRG1 -Location "East US" -IpConfigurations $gwipconfig -GatewayType "Vpn" -VpnType "RouteBased" -GatewaySku VpnGw2 -VpnGatewayGeneration "Generation2"
+$VirtualNetworkGatewayName = "VNet1GW"
+$Location = "East US"
+$GatewayType = "Vpn"
+$VpnType = "RouteBased"
+$GatewaySku = "VpnGw2"
+$VpnGatewayGeneration = "Generation2"
+New-AzVirtualNetworkGateway -Name $VirtualNetworkGatewayName -ResourceGroupName $ResourceGroupName -Location $Location -IpConfigurations $gwipconfig -GatewayType $GatewayType -VpnType $VpnType -GatewaySku $GatewaySku -VpnGatewayGeneration $VpnGatewayGeneration
 
-# Generally, it takes 45-50 mins to deploy this resource.
+# Deployment of this resource may take 45-50 mins.
