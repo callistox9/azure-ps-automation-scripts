4d3bbe376036b5bea802cca28de7f2cb38018aa8
Soumya Sourav Guha <codecallisto@gamil.com>
2024-06-07 22:39:48 +0530

    s2s_initial

---
+++ VPN_IaC_powershell/VPN_S2S.ps1
@@ -0,0 +1,26 @@
+# Organized script to deploy virtual network gateway in azure
+
+# Import required modules
+Import-Module Az.Resources
+Import-Module Az.Network
+$SubscriptionId = "xxxx-xxxx-xxxx"
+# Connect to azure account
+Connect-AzAccount -UseDeviceAuthentication -SubscriptionId $SubscriptionId
+# Verify the subscription
+Set-AzContext -Subscription $SubscriptionId
+
+# Create resource group if does not exist
+$ResourceGroupName = "TestRG1"
+$Location = "EastUS"
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+# Create Virtual Network And Add Subnet, if does not exist
+$VirtualNetworkName = "VNet1"
+$AddressPrefix = "10.1.0.0/16"
+$SubnetName = "GatewaySubnet"
+$SubnetAddressPrefix = "10.1.255.0/27"
+$SubnetName2 = "Frontend"
+$SubnetAddressPrefix2 = "10.1.0.0/24"
+$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Location $Location -Name $VirtualNetworkName -AddressPrefix $AddressPrefix
+Add-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix -VirtualNetwork $virtualNetwork | Set-AzVirtualNetwork
+Add-AzVirtualNetworkSubnetConfig -Name $SubnetName2 -AddressPrefix $SubnetAddressPrefix2 -VirtualNetwork $virtualNetwork | Set-AzVirtualNetwork
