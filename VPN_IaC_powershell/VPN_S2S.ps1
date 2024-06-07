48ff631332b8735b219ba1b620a7ece4ca56d80e
Soumya Sourav Guha <codecallisto@gamil.com>
2024-06-07 23:28:34 +0530

    s2s_VPN_Configured

--- VPN_IaC_powershell/VPN_S2S.ps1
+++ VPN_IaC_powershell/VPN_S2S.ps1
@@ -1,3 +1,5 @@
+# As there are multiple variables, dot-sourcing to store values for different variables 
+
 # Organized script to deploy virtual network gateway in azure
 
 # Import required modules
@@ -25,12 +27,60 @@ $virtualNetwork = New-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Lo
 Add-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix -VirtualNetwork $virtualNetwork | Set-AzVirtualNetwork
 Add-AzVirtualNetworkSubnetConfig -Name $SubnetName2 -AddressPrefix $SubnetAddressPrefix2 -VirtualNetwork $virtualNetwork | Set-AzVirtualNetwork
 
-$LocalNetworkGatewayName = "LocalSite1"
+# NOTE - The name of subnet should be exactly the same as it is necessary for VPN to work
+
+# Creating Local Network Gateway
 
 #The GatewayIPAddress is the IP address of your on-premises VPN device.
 #The AddressPrefix is your on-premises address space.
+
+$LocalNetworkGatewayName = "LocalSite1"
+
 
 $LocalNetworkGatewayAddressPrefix = "244.178.44.111/24"
 $OnPremiseAddressSpace = "244.178.44.111/24"
 
 New-AzLocalNetworkGateway -Name $LocalNetworkGatewayName -ResourceGroupName $ResourceGroupName -Location  -GatewayIpAddress $LocalNetworkGatewayAddressPrefix -AddressPrefix $OnPremiseAddressSpace 
+
+#Multiple -AddressPrefixe @(244.178.44.111/24, 244.178.44.111/24)
+
+# PublicIPAddress
+$PublicIpAddressName = "VNet1GWIP"
+$gwpip = New-AzPublicIpAddress -Name $PublicIpAddressName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Static -Sku Standard
+
+# Creating GatewayIp address configuration
+$vnet = Get-AzVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName
+$subnet = Get-AzVirtualNetworkSubnetConfig -Name $SubnetName -VirtualNetwork $vnet
+$GatewayIpConfigName = "gwipconfig1"
+$gwipconfig = New-AzVirtualNetworkGatewayIpConfig -Name $GatewayIpConfigName -SubnetId $subnet.Id -PublicIpAddressId $gwpip.Id
+
+# Creating VPN gateway
+$VirtualNetworkGatewayName = "VNet1GW"
+$Location = "East US"
+$GatewayType = "Vpn"
+$VpnType = "RouteBased"
+$GatewaySku = "VpnGw2"
+$VpnGatewayGeneration = "Generation2"
+New-AzVirtualNetworkGateway -Name $VirtualNetworkGatewayName -ResourceGroupName $ResourceGroupName -Location $Location -IpConfigurations $gwipconfig -GatewayType $GatewayType -VpnType $VpnType -GatewaySku $GatewaySku -VpnGatewayGeneration $VpnGatewayGeneration
+
+# Deployment of this resource may take 45-50 mins.
+
+#Configuring on-premise VPN device
+
+Get-AzPublicIpAddress -Name $PublicIpAddressName -ResourceGroupName $ResourceGroupName
+
+$gateway1 = Get-AzVirtualNetworkGateway -Name $VirtualNetworkGatewayName -ResourceGroupName $ResourceGroupName
+$local = Get-AzLocalNetworkGateway -Name $LocalNetworkGatewayName -ResourceGroupName  $ResourceGroupName
+
+#Create Connection
+
+$gatewayConnectionName = "VNet1toSite1"
+$location = "East US"
+$connectionType = "IPsec"
+$sharedKey = "abc123"
+
+New-AzVirtualNetworkGatewayConnection -Name $gatewayConnectionName -ResourceGroupName $ResourceGroupName -Location $location -VirtualNetworkGateway1 $gateway1 -LocalNetworkGateway2 $local -ConnectionType $connectionType -SharedKey $sharedKey
+
+# Verify the Connection
+Get-AzVirtualNetworkGatewayConnection -Name $gatewayConnectionName -ResourceGroupName $ResourceGroupName
+
